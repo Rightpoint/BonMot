b5c026a6ad6541ae294330e22b457d6b4fda9ffc
Zev Eisenberg <zev.eisenberg@raizlabs.com>
2015-04-20 17:09:56 -0400

    Figure style example.

--- Example/manuscript/Cells/RZFigureStyleCell.m
+++ Example/manuscript/Cells/RZFigureStyleCell.m
@@ -10,6 +10,8 @@
 
 #import <manuscript/RZManuscript.h>
 
+static NSString* const kRZFontNameEBGaramond = @"EBGaramond12-Regular";
+
 @interface RZFigureStyleCell ()
 
 @property (weak, nonatomic) IBOutlet UILabel *proportionalLiningLabel;
@@ -30,7 +32,20 @@
 {
     [super awakeFromNib];
 
-    
+    NSString *proportionalLiningString = self.proportionalLiningLabel.text;
+    NSString *proportionalOldstyleString = self.proportionalOldstyleLabel.text;
+    NSString *tabularLiningString = self.tabularLiningLabel.text;
+    NSString *tabularOldstyleString = self.tabularOldstyleLabel.text;
+
+    UIFont *ebGaramond = [UIFont fontWithName:kRZFontNameEBGaramond size:17.0f];
+
+    self.proportionalLiningLabel.attributedText = RZManuscript.font(ebGaramond).string(proportionalLiningString).figureSpacing(RZFigureSpacingProportional).figureCase(RZFigureCaseLining).write;
+
+    self.proportionalOldstyleLabel.attributedText = RZManuscript.font(ebGaramond).string(proportionalOldstyleString).figureSpacing(RZFigureSpacingProportional).figureCase(RZFigureCaseOldstyle).write;
+
+    self.tabularLiningLabel.attributedText = RZManuscript.font(ebGaramond).string(tabularLiningString).figureSpacing(RZFigureSpacingTabular).figureCase(RZFigureCaseLining).write;
+
+    self.tabularOldstyleLabel.attributedText = RZManuscript.font(ebGaramond).string(tabularOldstyleString).figureSpacing(RZFigureSpacingTabular).figureCase(RZFigureCaseOldstyle).write;
 }
 
 @end
