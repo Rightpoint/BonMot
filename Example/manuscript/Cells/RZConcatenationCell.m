f6d4ca0a74d67ac8c11f0f8df0c32c1c9fd335af
Zev Eisenberg <zev.eisenberg@raizlabs.com>
2015-05-20 20:30:40 -0400

    Demonstrate manuscript chaining and special handling of tracking with concatenation cell.

--- Example/manuscript/Cells/RZConcatenationCell.m
+++ Example/manuscript/Cells/RZConcatenationCell.m
@@ -10,9 +10,13 @@
 
 #import <manuscript/RZChainLink.h>
 
+static const CGFloat kRZColorAlpha = 0.3f;
+static const NSInteger kRZTracking = 200;
+
 @interface RZConcatenationCell ()
 
-@property (weak, nonatomic) IBOutlet UILabel *label;
+@property (weak, nonatomic) IBOutlet UILabel *centeredSmartTrackingLabel;
+@property (weak, nonatomic) IBOutlet UILabel *appendedSmartTrackingLabel;
 
 @end
 
@@ -27,7 +31,14 @@
 {
     [super awakeFromNib];
 
-    
+    RZManuscript *centeredSmartTracking = RZCursive.string(@"CENTERED WITH TRACKING").adobeTracking(kRZTracking).backgroundColor([[UIColor redColor] colorWithAlphaComponent:kRZColorAlpha]).manuscript;
+
+    RZManuscript *appended = RZCursive.string(@"APPENDED").adobeTracking(kRZTracking).backgroundColor([[UIColor greenColor] colorWithAlphaComponent:kRZColorAlpha]).manuscript;
+    RZManuscript *smartTracking = RZCursive.string(@" SMART TRACKING").adobeTracking(kRZTracking).backgroundColor([[UIColor blueColor] colorWithAlphaComponent:kRZColorAlpha]).manuscript;
+    appended.nextManuscript = smartTracking;
+
+    self.centeredSmartTrackingLabel.attributedText = centeredSmartTracking.attributedString;
+    self.appendedSmartTrackingLabel.attributedText = appended.attributedString;
 }
 
 @end
