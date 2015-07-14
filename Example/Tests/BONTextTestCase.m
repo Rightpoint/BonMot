2dc7618e4d6e389a752069bfbb5c9fbc1dda9802
Zev Eisenberg <zev.eisenberg@raizlabs.com>
2015-07-13 22:15:44 -0400

    Clean up test.

--- Example/Tests/BONTextTestCase.m
+++ Example/Tests/BONTextTestCase.m
@@ -7,7 +7,6 @@
 //
 
 #import "BONBaseTestCase.h"
-@import UIKit;
 
 #import <BonMot/BonMot.h>
 
@@ -16,18 +15,6 @@
 @end
 
 @implementation BONTextTestCase
-
-- (void)setUp
-{
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-}
-
-- (void)tearDown
-{
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
-}
 
 - (void)testText
 {
