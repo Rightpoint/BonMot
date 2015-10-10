07aed89c25a5ce4edcb6c1511c7ede0cc3049ba6
Zev Eisenberg <zev.eisenberg@raizlabs.com>
2015-10-09 20:15:49 -0400

    Update documentation comment.

--- Pod/Classes/BONText.h
+++ Pod/Classes/BONText.h
@@ -41,7 +41,7 @@ typedef NS_ENUM(NSUInteger, BONFigureSpacing) {
 @property (strong, nonatomic) UIColor *textColor;
 @property (strong, nonatomic) UIColor *backgroundColor;
 
-// adobeTracking and pointTracking are two interpretations of the same unit. If both are nonzero, Adobe tracking takes precedence.
+// adobeTracking and pointTracking are two interpretations of the same unit. They cannot both be set - if one is set to any value, the other is set to 0.
 @property (assign, nonatomic) NSInteger adobeTracking;
 @property (assign, nonatomic) CGFloat pointTracking;
 
