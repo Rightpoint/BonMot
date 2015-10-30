d406451616872cfa4dc1ac8aba3e7bc8684988fc
Zev Eisenberg <igeek1@gmail.com>
2015-10-30 11:43:29 -0400

    Tidying.

---
+++ Example/Tests/BONChainEmptyStringTestCase.m
@@ -0,0 +1,23 @@
+//
+//  BonChainCrashingTestCase.m
+//  BonMot
+//
+//  Created by Michael Skiba on 10/21/15.
+//  Copyright © 2015 Zev Eisenberg. All rights reserved.
+//
+
+#import "BONBaseTestCase.h"
+#import "BONChain.h"
+
+@interface BONChainEmptyStringTestCase : BONBaseTestCase
+
+@end
+
+@implementation BONChainEmptyStringTestCase
+
+- (void)testEmptyString
+{
+    XCTAssertNoThrow(BONChain.new.string(@"").attributedString);
+}
+
+@end
