73c19a2657457fc407616a0973efec6475602307
Nora Trapp <me@trappdesign.net>
2016-03-04 13:54:23 -0800

    Update example project Pods folder.

--- Example/Pods/Target Support Files/BonMot/BonMot-umbrella.h
+++ Example/Pods/Target Support Files/BonMot/BonMot-umbrella.h
@@ -8,6 +8,9 @@
 #import "BONText.h"
 #import "BONTextAlignmentConstraint.h"
 #import "UIImage+BonMotUtilities.h"
+#import "UILabel+BonMotUtilities.h"
+#import "UITextField+BonMotUtilities.h"
+#import "UITextView+BonMotUtilities.h"
 
 FOUNDATION_EXPORT double BonMotVersionNumber;
 FOUNDATION_EXPORT const unsigned char BonMotVersionString[];
