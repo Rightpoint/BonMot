aba18d09eced157783b743cff338dda0069a96d8
Zev Eisenberg <igeek1@gmail.com>
2016-04-20 14:21:49 -0400

    Add nullability annotations to UIKit utilities.

--- Pod/UIKit/Classes/UILabel+BonMotUtilities.h
+++ Pod/UIKit/Classes/UILabel+BonMotUtilities.h
@@ -8,6 +8,8 @@
 
 @import UIKit;
 
+#import "BONCompatibility.h"
+
 @protocol BONTextable;
 
 @interface UILabel (BonMotUtilities)
@@ -15,13 +17,13 @@
 /**
  *  Assign a @c BONTextable object to apply to the label text. When a new value is assigned to @c bonString the chain attributes will be applied.
  */
-@property (copy, nonatomic) id<BONTextable> bonTextable;
+@property (copy, nonatomic, BONNullable) id<BONTextable> bonTextable;
 
 /**
  *  Assigning a string via this method will apply the attributes of any assigned @c BONTextable and set the @c attributedText with the resulting @c NSAttributedString.
  *
  *  @param string The text to be displayed.
  */
-- (void)setBonString:(NSString *)string;
+- (void)setBonString:(BONNullable NSString *)string;
 
 @end
