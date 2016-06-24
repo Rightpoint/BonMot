de81d1050239526738b5772c99e20f6b57221aba
Phil Larson <phil@sportle.tv>
2016-06-24 07:53:29 -0700

    Add BONLinkTestCase

---
+++ Example/Tests/BONLinkTestCase.m
@@ -0,0 +1,46 @@
+//
+//  BONLinktestCase.m
+//  BonMot
+//
+//  Created by Phil Larson on 6/24/16.
+//  Copyright © 2016 Zev Eisenberg. All rights reserved.
+//
+
+#import "BONBaseTestCase.h"
+
+@import BonMot;
+
+@interface BONLinktestCase : BONBaseTestCase
+
+@end
+
+@implementation BONLinktestCase
+
+- (void)testNeither
+{
+    NSString *quote = @"Hello, world";
+    NSDictionary *attributes = BONChain.new.string(quote).attributes;
+    
+    XCTAssertNil(attributes[NSLinkAttributeName]);
+}
+
+- (void)testLink
+{
+    NSAttributedString *attributedString =
+    BONChain.new
+    .string(@"Hello, world")
+    .link([NSURL URLWithString:@"https://apple.com"])
+    .attributedString;
+    
+    XCTAssertEqualObjects(attributedString.string, @"Hello, world");
+
+    NSDictionary *controlAttributes = @{
+                                        BONValueFromRange(0, 12) : @{
+                                                NSLinkAttributeName : [NSURL URLWithString:@"https://apple.com"]
+                                                },
+                                        };
+    
+    BONAssertAttributedStringHasAttributes(attributedString, controlAttributes);
+}
+
+@end
