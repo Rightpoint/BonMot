22a7d29d5a70732e5261d70ffd55da9feb320e01
Brian King <brian.king@raizlabs.com>
2016-09-26 13:58:57 -0400

    Initial Playground

--- Playground/APIFeedback.playground/Contents.swift
+++ Playground/APIFeedback.playground/Contents.swift
@@ -1,5 +1,26 @@
-//: Playground - noun: a place where people can play
+// Open via BonMot.xcworkspace and build the BonMot-iOS target if the playground does not work.
 
+import UIKit
 import BonMot
 
-var str = "Hello, BonMot"
+let noSpaceStyle = BonMot(
+    .font(.systemFont(ofSize: 17)),
+    .adapt(.control),
+    .textColor(.darkGray)
+)
+
+let noSpaceString = NSMutableAttributedString(attributedStrings: [
+    ("barn", "This"),
+    ("bee", "string"),
+    ("bug", "is"),
+    ("circuit", "separated"),
+    ("cut", "by"),
+    ("discount", "images"),
+    ("gift", "and"),
+    ("pin", "no-break"),
+    ("robot", "spaces"),
+    ].map() { imageName, text in
+        let string = BonMot(.baselineOffset(-10)).attributedString(from: UIImage(named: imageName)!)
+        string.extend(with: text, style: noSpaceStyle)
+        return string
+}, separator: .text(Special.noBreakSpace.description))
