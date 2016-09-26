2dfb4b26687520ad17349cb71cf61f9f04602cd2
Brian King <brian.king@raizlabs.com>
2016-09-26 13:35:12 -0400

    Reorganize example project and add Playground

---
+++ Example/Pods/Target Support Files/Pods-BonMot-Example-iOS/Pods-BonMot-Example-iOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BonMot_Example_iOS : NSObject
+@end
+@implementation PodsDummy_Pods_BonMot_Example_iOS
+@end
---
+++ Playground/APIFeedback.playground/Contents.swift
@@ -0,0 +1,5 @@
+//: Playground - noun: a place where people can play
+
+import BonMot
+
+var str = "Hello, BonMot"
