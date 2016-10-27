6065aca61a407075ce19d640aad7e0ac225d1831
Brian King <brian.king@raizlabs.com>
2016-10-27 17:01:15 -0400

    Remove workspace and playgrounds

--- Playground/Features.playground/Pages/Welcome.xcplaygroundpage/Contents.swift
+++
@@ -1,8 +0,0 @@
-/*:
- # Welcome to BonMot
- This playground is here to introduce new developers to BonMot.
- - [Non Breaking Space](Non%20Breaking%20Space)
- - [Extending NSMutableAttributedString](Extending%NSMutableAttributedString)
- */
-
-//: [Next](@next)
--- Playground/Features.playground/Sources/Colors.swift
+++
@@ -1,8 +0,0 @@
-import UIKit
-import BonMot
-
-extension UIColor {
-    public static var raizlabsRed: UIColor {
-        return UIColor(red: 0.927, green: 0.352, blue: 0.303, alpha: 1.0)
-    }
-}
