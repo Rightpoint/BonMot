1aed67c1b977d59420f423eb0b8fe4dc124b7484
Zev Eisenberg <zev.eisenberg@raizlabs.com>
2016-10-29 00:53:39 -0400

    Add hex utility for UIColor in example app.

--- Example-iOS/AppDelegate.swift
+++ Example-iOS/AppDelegate.swift
@@ -40,8 +40,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 extension UIColor {
     static var raizlabsRed: UIColor {
-        return UIColor(red: 0.927, green: 0.352, blue: 0.303, alpha: 1.0)
+        return UIColor(hex: 0xEC594D)
     }
+
+    convenience init(hex: UInt32, alpha: CGFloat = 1) {
+        self.init(
+            red: CGFloat((hex >> 16) & 0xff) / 255.0,
+            green: CGFloat((hex >> 8) & 0xff) / 255.0,
+            blue: CGFloat(hex & 0xff) / 255.0,
+            alpha: alpha)
+    }
+
 }
 
 extension UIFont {
