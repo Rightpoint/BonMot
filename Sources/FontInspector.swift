5ea716a0f2528da6c08bfa9bde9cad3bd8acd251
Zev Eisenberg <igeek1@gmail.com>
2016-11-03 11:16:58 -0400

    Add font inspector to query fonts about OpenType features.

---
+++ Sources/FontInspector.swift
@@ -0,0 +1,169 @@
+//
+//  FontInspector.swift
+//  BonMot
+//
+//  Created by Zev Eisenberg on 11/2/16.
+//  Copyright © 2016 Raizlabs. All rights reserved.
+//
+
+// This is not supported by watchOS
+#if os(iOS) || os(tvOS) || os(OSX)
+    import Foundation
+    import CoreText
+
+#if swift(>=3.0)
+    typealias FontFeatureDictionary = [String : Any]
+    typealias BaseType = Any
+    #else
+    typealias FontFeatureDictionary = [String : AnyObject]
+    typealias BaseType = AnyObject
+#endif
+
+    public extension BONFont {
+
+        public func has(feature theFeature: MappableFeature) -> Bool {
+            let matchingFeatures = availableFeaturesDictionaries.filter { featureDict in
+                guard let featureTypeIdentifier = featureDict[kCTFontFeatureTypeIdentifierKey as String] as? Int else {
+                    return false
+                }
+
+                return theFeature.featureTypeIdentifiers.contains(featureTypeIdentifier)
+            }
+
+            return !matchingFeatures.isEmpty
+        }
+
+        var availableFontFeatures: String {
+            let preamble = "Available font features of \(fontName)"
+            return preamble + availableFeaturesDictionaries.map { featureDict in
+                let featureTypeIdentifier = featureDict[kCTFontFeatureTypeIdentifierKey as String] as? Int
+                let featureName = featureDict[kCTFontFeatureTypeNameKey as String] as? String
+                let exclusive = featureDict[kCTFontFeatureTypeExclusiveKey as String] as? Bool ?? false
+                let selectors = featureDict[kCTFontFeatureTypeSelectorsKey as String] as? [FontFeatureDictionary] ?? []
+
+                var resultString = ""
+                resultString += "\(featureName ?? "[unknown feature]") - feature type identifier: \(featureTypeIdentifier.map(String.init(describing:)) ?? "[unknown feature identifier]")"
+                resultString += "\n    Exclusive: \(exclusive)"
+                if !selectors.isEmpty {
+                    resultString += "\n    Selectors:"
+                    resultString += selectors.map { selectorDict in
+                        let selectorIdentifier = selectorDict[kCTFontFeatureSelectorIdentifierKey as String] as? Int
+                        let selectorName = selectorDict[kCTFontFeatureSelectorNameKey as String] as? String
+                        let selectorIsDefault = selectorDict[kCTFontFeatureSelectorDefaultKey as String] as? Bool ?? false
+
+                        var resultString = ""
+                        resultString += "    * \(selectorName ?? "[unknown selector]") - selector: \(selectorIdentifier.map(String.init(describing:)) ?? "[unknownSelectorIdentifier]")"
+                        if selectorIsDefault {
+                            resultString += " (default)"
+                        }
+                        return resultString
+                        }.reduce("") { $0 + "\n" + $1 }
+                }
+                return resultString
+                }.reduce("") { $0 + "\n\n" + $1 }
+        }
+
+    }
+
+    private extension BONFont {
+
+        var availableFeaturesDictionaries: [FontFeatureDictionary] {
+            let coreTextFont = CTFontCreateWithName(
+                fontName as CFString,
+                pointSize,
+                nil)
+
+            guard let features = CTFontCopyFeatures(coreTextFont) else {
+                return []
+            }
+
+            #if swift(>=3.0)
+                guard let typedFeatures = features as? [FontFeatureDictionary] else {
+                    fatalError("failed to convert to \([FontFeatureDictionary].self) from \(features)")
+                }
+                return typedFeatures
+            #else
+                let partlyUnwrappedArray = features as [BaseType]
+
+                guard let typedFeatures = partlyUnwrappedArray as? [FontFeatureDictionary] else {
+                    fatalError()
+                }
+
+                return typedFeatures
+            #endif
+        }
+
+    }
+
+    /// Home-grown mapping of font features. Best documentation on the subject I could find is on [this Apple discussion thread](http://lists.apple.com/archives/coretext-dev/2011/Jan/msg00004.html).
+    public enum FeatureTypeIdentifier: Int {
+
+        case allTypographicFeatures = 0
+        case ligatures = 1
+        case numberSpacing = 6
+        case verticalPosition = 10
+        case contextualFractionalForms = 11
+        case ornaments = 16
+        case stylisticVariants = 18
+        case numberCase = 21
+        case textSpacing = 22
+        case caseSensitiveLayout = 33
+        case stylisticAlternates = 35
+        case contextualAlternates = 36
+        case lowerCase = 37
+        case upperCase = 38
+        case special = 128
+
+    }
+
+    public protocol MappableFeature {
+        var featureTypeIdentifiers: [Int] { get }
+    }
+
+    extension NumberCase: MappableFeature {
+
+        public var featureTypeIdentifiers: [Int] {
+            return [FeatureTypeIdentifier.numberCase.rawValue]
+        }
+
+    }
+
+    extension NumberSpacing: MappableFeature {
+
+        public var featureTypeIdentifiers: [Int] {
+            return [FeatureTypeIdentifier.numberSpacing.rawValue]
+        }
+    }
+
+    extension VerticalPosition: MappableFeature {
+
+        public var featureTypeIdentifiers: [Int] {
+            return [FeatureTypeIdentifier.verticalPosition.rawValue]
+        }
+
+    }
+
+    extension SmallCaps: MappableFeature {
+
+        public var featureTypeIdentifiers: [Int] {
+            switch self {
+            case .disabled:
+                return [
+                    FeatureTypeIdentifier.upperCase.rawValue,
+                    FeatureTypeIdentifier.lowerCase.rawValue,
+                ]
+            case .fromUppercase: return [ FeatureTypeIdentifier.upperCase.rawValue ]
+            case .fromLowercase: return [ FeatureTypeIdentifier.lowerCase.rawValue ]
+            }
+        }
+
+    }
+
+    extension StylisticAlternates: MappableFeature {
+
+        public var featureTypeIdentifiers: [Int] {
+            return [ FeatureTypeIdentifier.stylisticAlternates.rawValue ]
+        }
+
+    }
+#endif
---
+++ Tests/FontInspectorTests.swift
@@ -0,0 +1,119 @@
+//
+//  FontInspectorTests.swift
+//  BonMot
+//
+//  Created by Zev Eisenberg on 11/2/16.
+//  Copyright © 2016 Raizlabs. All rights reserved.
+//
+
+import XCTest
+@testable import BonMot
+
+class FontInspectorTests: XCTestCase {
+
+    let systemFont = BONFont.systemFont(ofSize: 24)
+    let garamond: BONFont = {
+        EBGaramondLoader.loadFontIfNeeded()
+        return BONFont(name: "EBGaramond12-Regular", size: 24)!
+    }()
+
+    override func setUp() {
+        super.setUp()
+        EBGaramondLoader.loadFontIfNeeded()
+    }
+
+    func testHasFeature() {
+        XCTAssertTrue(systemFont.has(feature: SmallCaps.fromLowercase))
+        XCTAssertTrue(systemFont.has(feature: SmallCaps.disabled))
+        XCTAssertFalse(systemFont.has(feature: NumberCase.lower))
+        XCTAssertTrue(garamond.has(feature: NumberCase.lower))
+    }
+
+    //swiftlint:disable:next function_body_length
+    func testAvailableFeatures() {
+        let garamondControlString = [
+            "Available font features of EBGaramond12-Regular",
+            "",
+            "All Typographic Features - feature type identifier: 0",
+            "    Exclusive: false",
+            "    Selectors:",
+            "    * On - selector: 0 (default)",
+            "",
+            "Ligatures - feature type identifier: 1",
+            "    Exclusive: false",
+            "    Selectors:",
+            "    * Common Ligatures - selector: 2 (default)",
+            "    * Rare Ligatures - selector: 4",
+            "    * Historical Ligatures - selector: 22",
+            "",
+            "Number Spacing - feature type identifier: 6",
+            "    Exclusive: true",
+            "    Selectors:",
+            "    * Monospaced Numbers - selector: 0",
+            "    * Proportional Numbers - selector: 1",
+            "    * No Change - selector: 4 (default)",
+            "",
+            "Vertical Position - feature type identifier: 10",
+            "    Exclusive: true",
+            "    Selectors:",
+            "    * Normal Vertical Position - selector: 0 (default)",
+            "    * Superiors/Superscripts - selector: 1",
+            "    * Inferiors/Subscripts - selector: 2",
+            "    * Ordinals - selector: 3",
+            "    * Scientific Inferiors - selector: 4",
+            "",
+            "Contextual Fractional Forms - feature type identifier: 11",
+            "    Exclusive: true",
+            "    Selectors:",
+            "    * No Fractional Forms - selector: 0 (default)",
+            "    * Diagonal - selector: 2",
+            "",
+            "Number Case - feature type identifier: 21",
+            "    Exclusive: true",
+            "    Selectors:",
+            "    * Old-Style Figures - selector: 0",
+            "    * Lining Figures - selector: 1",
+            "    * No Change - selector: 2 (default)",
+            "",
+            "Text Spacing - feature type identifier: 22",
+            "    Exclusive: true",
+            "    Selectors:",
+            "    * No Change - selector: 7 (default)",
+            "    * No Kerning - selector: 8",
+            "",
+            "Case-Sensitive Layout - feature type identifier: 33",
+            "    Exclusive: false",
+            "    Selectors:",
+            "    * Capital Forms - selector: 0",
+            "",
+            "Alternative Stylistic Sets - feature type identifier: 35",
+            "    Exclusive: false",
+            "    Selectors:",
+            "    * Cyrillic alternate de, el and elj - selector: 2",
+            "    * Stylistic Set 2 - selector: 4",
+            "    * Stylistic Set 5 - selector: 10",
+            "    * Stylistic Set 6 - selector: 12",
+            "    * Stylistic Set 7 - selector: 14",
+            "    * Stylistic Set 20 - selector: 40",
+            "",
+            "Contextual Alternates - feature type identifier: 36",
+            "    Exclusive: false",
+            "    Selectors:",
+            "    * Contextual Alternates - selector: 0 (default)",
+            "",
+            "Lower Case - feature type identifier: 37",
+            "    Exclusive: true",
+            "    Selectors:",
+            "    * No Change - selector: 0 (default)",
+            "    * Small Capitals - selector: 1",
+            "",
+            "Upper Case - feature type identifier: 38",
+            "    Exclusive: true",
+            "    Selectors:",
+            "    * No Change - selector: 0 (default)",
+            "    * Small Capitals - selector: 1",
+            ].joined(separator: "\n")
+        XCTAssertEqual(garamond.availableFontFeatures, garamondControlString)
+    }
+
+}
