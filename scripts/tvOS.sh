fb23e1ef5e7641bd9fde03c5b652f4e9c247c84d
Derek Ostrander <djostran@gmail.com>
2016-11-03 10:03:31 -0700

    CircleCI Migration - tvOS use id's

--- scripts/tvOS.sh
+++ scripts/tvOS.sh
@@ -5,7 +5,7 @@ set -o pipefail && \
   -project BonMot.xcodeproj \
   -scheme BonMot-tvOS \
   -sdk appletvsimulator \
-  -destination "name=Apple TV 1080p" \
+  -destination "id=48B0E1AB-F5EB-40FB-9372-A16B93349B12" \
   CODE_SIGNING_REQUIRED=NO \
   CODE_SIGN_IDENTITY= \
   | xcpretty
@@ -15,7 +15,7 @@ set -o pipefail && TOOLCHAINS=com.apple.dt.toolchain.Swift_2_3 && \
   -project BonMot.xcodeproj \
   -scheme BonMot-tvOS \
   -sdk appletvsimulator \
-  -destination "name=Apple TV 1080p" \
+  -destination "id=48B0E1AB-F5EB-40FB-9372-A16B93349B12" \
   CODE_SIGNING_REQUIRED=NO \
   CODE_SIGN_IDENTITY= \
   | xcpretty
