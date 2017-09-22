78df55e4f5f535dc6dbe4a9ac2d675d9b1fffb57
Zev Eisenberg <zev@zeveisenberg.com>
2017-09-21 22:42:00 -0400

    Update tvOS build scripts for latest CircleCI changes.

--- scripts/tvOS-11.0.sh
+++ scripts/tvOS-11.0.sh
@@ -5,7 +5,7 @@ set -o pipefail && \
   -project BonMot.xcodeproj \
   -scheme BonMot-tvOS \
   -sdk appletvsimulator \
-  -destination "platform=tvOS Simulator,name=Apple TV 1080p,OS=11.0" \
+  -destination "platform=tvOS Simulator,name=Apple TV,OS=11.0" \
   CODE_SIGNING_REQUIRED=NO \
   CODE_SIGN_IDENTITY= \
   | bundle exec xcpretty
