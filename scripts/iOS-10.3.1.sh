118b1ab91b85f4ac082bacc6bd1f96c67668323e
Zev Eisenberg <zev@zeveisenberg.com>
2017-09-21 22:40:52 -0400

    Run iOS 10 tests on 10.3.1 beacuse it is available on CircleCI.

--- scripts/iOS-10.3.1.sh
+++ scripts/iOS-10.3.1.sh
@@ -5,7 +5,7 @@ set -o pipefail && \
   -project BonMot.xcodeproj \
   -scheme BonMot-iOS \
   -sdk iphonesimulator \
-  -destination "platform=iOS Simulator,name=iPhone 6S,OS=10.2" \
+  -destination "platform=iOS Simulator,name=iPhone 6S,OS=10.3.1" \
   CODE_SIGNING_REQUIRED=NO \
   CODE_SIGN_IDENTITY= \
   | bundle exec xcpretty
