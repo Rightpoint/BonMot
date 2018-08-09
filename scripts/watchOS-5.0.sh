46ac76d84384ddf4f9e395b8171002a4bc3c16ea
Zev Eisenberg <zev@zeveisenberg.com>
2018-08-09 19:49:32 -0400

    Fix watchOS simulator name.

--- scripts/watchOS-5.0.sh
+++ scripts/watchOS-5.0.sh
@@ -5,7 +5,7 @@ set -o pipefail && \
   -project BonMot.xcodeproj \
   -scheme BonMot-watchOS \
   -sdk watchsimulator \
-  -destination "platform=watchOS Simulator,name=Apple Watch - 38mm,OS=5.0" \
+  -destination "platform=watchOS Simulator,name=Apple Watch Series 3 - 38mm,OS=5.0" \
   CODE_SIGNING_REQUIRED=NO \
   CODE_SIGN_IDENTITY= \
   | bundle exec xcpretty
