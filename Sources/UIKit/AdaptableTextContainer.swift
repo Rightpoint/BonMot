e8ef86a2b373bcc28c20ebc7005403d2687f06be
Ryan Zulkoski <ryan.zulkoski@buildertrend.com>
2020-12-18 11:08:46 -0600

    Reverting back to returning optional StyleAttributes from UISegmentedControl extension method bon_titleTextAttributes(for:) since it is internal API.

--- Sources/UIKit/AdaptableTextContainer.swift
+++ Sources/UIKit/AdaptableTextContainer.swift
@@ -146,8 +146,8 @@ extension UISegmentedControl {
 
     // `UISegmentedControl` has terrible generics ([NSObject: AnyObject]? or [AnyHashable: Any]?) on
     /// `titleTextAttributes`, so use a helper in Swift 3+
-    @nonobjc final func bon_titleTextAttributes(for state: UIControl.State) -> StyleAttributes {
-		let attributes = titleTextAttributes(for: state) ?? [:]
+    @nonobjc final func bon_titleTextAttributes(for state: UIControl.State) -> StyleAttributes? {
+        guard let attributes = titleTextAttributes(for: state) else { return nil }
         #if swift(>=4.2)
             return attributes
         #else
@@ -168,7 +168,7 @@ extension UISegmentedControl {
     @objc(bon_updateTextForTraitCollection:)
     public func adaptText(forTraitCollection traitCollection: UITraitCollection) {
         for state in UIControl.State.commonStates {
-            let attributes = bon_titleTextAttributes(for: state)
+            guard let attributes = bon_titleTextAttributes(for: state) else { continue }
             let newAttributes = NSAttributedString.adapt(attributes: attributes, to: traitCollection)
             setTitleTextAttributes(newAttributes, for: state)
         }
